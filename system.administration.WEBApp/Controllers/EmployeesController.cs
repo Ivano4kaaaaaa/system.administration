20e3b6bbeb7dc715982ab93ebf9e10f684dd3e0d
Ivano4kaaaaaa <i.Melko20@dpl.ukr.education>
2024-11-01 20:06:30 +0200

    Add WebApp and EmployeesController

---
+++ system.administration.WEBApp/Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using system.administration.DAL.Entities;
+
+namespace system.administration.WEBApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employees> employees = new List<Employees>();
+            employees.Add(new Employees() { employee_id = 1, gender = "чоловік" });
+            employees.Add(new Employees() { employee_id = 2, gender = "жінка" });
+
+            return View(employees);
+        }
+        public IActionResult AddEmployees()
+        {
+         
+            return View();
+        }
+        //public IActionResult AddEmployees(Employees employees)
+        //{
+        //    return View();
+        //}
+    }
+
+}
---
+++ system.administration.WEBApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace system.administration.WEBApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
