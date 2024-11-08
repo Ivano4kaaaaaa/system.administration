a489d6b2cd2e8bc7e6fad441c6f254680521ad7e
Ivano4kaaaaaa <i.Melko20@dpl.ukr.education>
2024-11-08 18:02:44 +0200

    Update controller Employees and Performance

--- system.administration.WEBApp/Controllers/EmployeesController.cs
+++ system.administration.WEBApp/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using system.administration.DAL.Entities;
 using systemadministration.BLL.Services;
 
@@ -6,21 +7,25 @@ namespace system.administration.WEBApp.Controllers
 {
     public class EmployeesController : Controller
     {
-        private readonly EmployeessServices _employeeServices;  
+        private readonly EmployeessServices _employeeServices;
+
         public EmployeesController(EmployeessServices employeesServices)
-        { 
+        {
             _employeeServices = employeesServices;
         }
+
         public IActionResult Index()
-        { 
-        var employees  = _employeeServices.GetEmployeesAsync().Result;
+        {
+            var employees = _employeeServices.GetEmployeesAsync().Result;
             return View(employees);
         }
+
         public IActionResult AddEmployees(Employees employee)
         {
-            _employeeServices.AddEmployeesAsync( employee); 
+            _employeeServices.AddEmployeesAsync(employee);
             return RedirectToAction(nameof(Index));
         }
+
         public async Task<IActionResult> DeleteEmployeesAsync(int id)
         {
             var employee = await _employeeServices.GetEmployeesByIdAsync(id);
@@ -33,8 +38,7 @@ namespace system.administration.WEBApp.Controllers
             return NoContent();
         }
 
-      
-        public async Task<ActionResult> UpdateEmployee(int id, Employees employee)
+        public async Task<IActionResult> UpdateEmployee(int id, Employees employee)
         {
             if (id != employee.id)
             {
@@ -45,6 +49,4 @@ namespace system.administration.WEBApp.Controllers
             return NoContent();
         }
     }
-
 }
-
--- system.administration.WEBApp/Controllers/PerformanceController.cs
+++ system.administration.WEBApp/Controllers/PerformanceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using system.administration.DAL.Entities;
 using systemadministration.BLL.Services;
 
@@ -7,45 +8,46 @@ namespace system.administration.WEBApp.Controllers
     public class PerformanceController : Controller
     {
         private readonly PerformanceeServices _performanceeServices;
+
+        public PerformanceController(PerformanceeServices performanceeServices)
+        {
+            _performanceeServices = performanceeServices;
+        }
+
         public IActionResult Index()
         {
-            var performance = _performanceeServices.GetPerformanceAsync().Result;
+            var performance = _performanceeServices.GetPerformanceeAsync().Result;
             return View(performance);
         }
-    
-    public PerformanceController(PerformanceeServices performanceeServices)
-    {
-        _performanceeServices = performanceeServices;
-    }
-   
-    public IActionResult AddPerformance(Performance performance)
-    {
+
+        public IActionResult AddPerformance(Performance performance)
+        {
             _performanceeServices.AddPerformanceAsync(performance);
-        return RedirectToAction(nameof(Index));
-    }
-    public async Task<IActionResult> DeleteEmployeesAsync(int id)
-    {
-        var performance = await _performanceeServices.GetPerformanceByIdAsync(id);
-        if (performance  == null)
-        {
-            return NotFound();
+            return RedirectToAction(nameof(Index));
         }
 
-        await _performanceeServices.DeletePerformanceAsync(performance);
-        return NoContent();
-    }
-
-
-    public async Task<ActionResult> UpdatePerformance(int id, Performance performance)
-    {
-        if (id != performance.performance_id)
+        public async Task<IActionResult> DeletePerformanceAsync(int id)
         {
-            return BadRequest();
+            var performance = await _performanceeServices.GetPerformanceByIdAsync(id);
+            if (performance == null)
+            {
+                return NotFound();
+            }
+
+            await _performanceeServices.DeletePerformanceAsync(performance);
+            return NoContent();
         }
 
-        await _performanceeServices.UpdatePerformance(performance);
-        return NoContent();
+        public async Task<IActionResult> UpdatePerformance(int id, Performance performance)
+        {
+            if (id != performance.performance_id)
+            {
+                return BadRequest();
+            }
+
+            await _performanceeServices.UpdatePerformance(performance);
+            return NoContent();
+        }
     }
-}
 }
 
